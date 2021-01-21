0b9ed973aea941a8675ee9f13b228d2d434409dc
omersin54 <67612854+omersin54@users.noreply.github.com>
2021-01-22 01:28:57 +0300

    Add files via upload

---
+++ Degisgenler/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety = tip güvenliği
+            //değer tutucu
+            //Do not repeat yourself
+            // Console.WriteLine(); cw tab tab
+            //if (true)
+            //{
+            //if tab tab
+            //}
+
+            string kategoriEtiketi = "Kategori";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = true;
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+
+            if (dolarDun > dolarBugun)
+            {
+                Console.WriteLine("Azalış butonu");
+            }
+            else if (dolarDun < dolarBugun)
+            {
+                Console.WriteLine("Artış butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi butonu");
+            }
+
+
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+
+            Console.WriteLine(faizOrani);
+
+            Console.WriteLine(ogrenciSayisi);
+
+            Console.WriteLine(kategoriEtiketi);
+
+        }
+    }
+}
+
+
+
+
+
---
+++ Donguler/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string kurs1="Ygyk";
+            //string kurs2="pbitk";
+            //string kurs3="java";
+
+            //array - dizi
+
+            string[] kurslar = new string[] { "ygyk", "pbitk", "java", "pyhton", "C++" };
+
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+            Console.WriteLine("For bitti");
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+
+
+
+            /* string ad;
+                       Console.Write("Adınızı Giriniz: ");
+                       ad = Console.ReadLine();
+
+                       Console.Write("Merhaba: " + ad);
+                       Console.ReadKey(); */
+
+            Console.WriteLine("Sayfa sonu");
+        }
+    }
+}
