1deb040042cf6fbaa6d2a9f60216e070a94b125a
omersin54 <67612854+omersin54@users.noreply.github.com>
2021-01-22 16:18:22 +0300

    3. Gün Ödevi

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Add(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + " eklendi.");
+        }
+        public void Delete(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + " silindi.");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Ad = "Ali Vehbi";
+            musteri1.Soyad = "Mersin";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Ad = "Emine";
+            musteri2.Soyad = "Mersin";
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Add(musteri1);
+            musteriManager.Add(musteri2);
+
+            Console.WriteLine("----------");
+
+            musteriManager.Delete(musteri1);
+            musteriManager.Delete(musteri2);
+
+            Console.WriteLine("----------");
+
+            string[] musteriler = new string[] { musteri1.Ad, musteri2.Ad};
+
+            for (int i = 0; i < musteriler.Length; i++)
+            {
+                Console.WriteLine(musteriler[i]);
+            }
+        }
+    }
+}
